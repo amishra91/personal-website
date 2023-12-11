5a9bef5a3ec150d7f15733d2378f45a11be2e8e2
Abhishek Mishra <amishra91@users.noreply.github.com>
2023-12-11 12:18:31 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -39,7 +39,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1IHSMXtIhxYWrZe1Bxl_OMqrO-JEHC9OV/view?usp=sharing"
+              href="https://drive.google.com/file/d/1Gs-DfOzQBKdbe0PLbnxQx-ViKWFEaeCO/view?usp=sharing"
               target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
